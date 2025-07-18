9b755d599c759bf9cd1123246d83d371ffb774e0
linhnq9904 <linhnqph46354@fpt.edu.vn>
2025-07-18 15:07:34 +0700

    nothing

---
+++ duanReact/src/App.tsx
@@ -0,0 +1,32 @@
+import ProductList from "./components/ProductList";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CategoryList from "./components/CategoryList";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <ProductList />,
+    },
+    {
+      path: "/products",
+      element: <ProductList />,
+    },
+    {
+      path: "/categories",
+      element: <CategoryList />,
+    },
+    {
+      path: "/order",
+      element: <CategoryList />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
