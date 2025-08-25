1d68c51c7a6397d1daf4697d233d09bf74d5bbc0
linhnq9904 <linhnqph46354@fpt.edu.vn>
2025-08-26 00:24:46 +0700

    sdsd

--- duanReact/src/components/Greeting.tsx
+++ duanReact/src/components/Greeting.tsx
@@ -13,4 +13,34 @@ function Greeting(props: Props) {
     );
 }
 
+export default Greeting; type Props = {
+    name: string;
+    count: number;
+    children?: React.ReactNode;
+};
+
+function Greeting(props: Props) {
+    return (
+        <>
+            <h1>Welcome to my app! {props.count} </h1>
+            <div> {props.children}</div>
+        </>
+    );
+}
+
+export default Greeting; type Props = {
+    name: string;
+    count: number;
+    children?: React.ReactNode;
+};
+
+function Greeting(props: Props) {
+    return (
+        <>
+            <h1>Welcome to my app! {props.count} </h1>
+            <div> {props.children}</div>
+        </>
+    );
+}
+
 export default Greeting;
